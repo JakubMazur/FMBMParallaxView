dd74596df2d682665db7d7356831f5515fc35e9d
Paweł Ferens <pawel.ferens@gmail.com>
2014-07-05 15:44:51 +0200

    content manager

---
+++ FMBMParallaxView/ContentManager.swift
@@ -0,0 +1,13 @@
+//
+//  ContentManager.swift
+//  FMBMParallaxView
+//
+//  Created by Pawel on 05.07.2014.
+//  Copyright (c) 2014 Kettu Jakub Mazur. All rights reserved.
+//
+
+import UIKit
+
+class ContentManager: NSObject {
+   
+}
