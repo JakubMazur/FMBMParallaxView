66f5706992159953940496dc17d6b2c0ebbe59f8
Kamil Burczyk <kamil.burczyk@sigmapoint.pl>
2014-07-05 17:06:15 +0200

    Added parallax image view

---
+++ FMBMParallaxView/FMBMImageView.swift
@@ -0,0 +1,27 @@
+//
+//  FMBMImageView.swift
+//  FMBMParallaxView
+//
+//  Created by Kamil Burczyk on 05.07.2014.
+//  Copyright (c) 2014 Kettu Jakub Mazur. All rights reserved.
+//
+
+import UIKit
+
+class FMBMImageView: UIImageView {
+
+    init(frame: CGRect) {
+        super.init(frame: frame)
+        // Initialization code
+    }
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect)
+    {
+        // Drawing code
+    }
+    */
+
+}
