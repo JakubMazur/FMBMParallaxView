47fda18b53743641c63bb7f9b33e7688f31bc807
Kuba Mazur <Kuba@MacBook-Pro-Kuba.local>
2014-07-05 16:13:15 +0200

    prepare storyboard and collection view

---
+++ FMBMParallaxView/ExampleCollectionViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  ExampleCollectionViewCell.swift
+//  FMBMParallaxView
+//
+//  Created by Kuba Mazur on 05.07.2014.
+//  Copyright (c) 2014 Kettu Jakub Mazur. All rights reserved.
+//
+
+import UIKit
+
+class ExampleCollectionViewCell: UICollectionViewCell {
+
+    init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    init(coder aDecoder: NSCoder!) {
+        super.init(coder: aDecoder)
+    }
+    
+}
---
+++ FMBMParallaxView/ExampleCollectionViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  FMBMParallaxView
+//
+//  Created by Kuba Mazur on 05.07.2014.
+//  Copyright (c) 2014 Kettu Jakub Mazur. All rights reserved.
+//
+
+import UIKit
+
+var imageDataSource = String[]()
+
+class ExampleCollectionViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    @IBOutlet var collectionView : UICollectionView
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.connectForData();
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+//MARK: - dataConnector
+    
+    func connectForData() {
+        
+        for (var i=0;i<500;i++) {
+            let foo = "http://lorempixel.com/400/200/sports/" + String(i) + "/"
+            imageDataSource.append(foo)
+            println(imageDataSource[i])
+        }
+        
+        collectionView.reloadData();
+
+    }
+    
+//MARK: - CollectionView delegate
+    
+    func collectionView(collectionView: UICollectionView!, numberOfItemsInSection section: Int) -> Int {
+        return imageDataSource.count;
+    }
+    
+    func collectionView(collectionView: UICollectionView!, cellForItemAtIndexPath indexPath: NSIndexPath!) -> UICollectionViewCell! {
+        
+        var cell = collectionView .dequeueReusableCellWithReuseIdentifier("MyExampleCell", forIndexPath: indexPath) as ExampleCollectionViewCell
+        //cell.imageView.image = UIImage(named:imageDataSource[indexPath.row])
+        cell.backgroundColor = UIColor.redColor()
+        return cell
+    }
+    
+
+}
+
