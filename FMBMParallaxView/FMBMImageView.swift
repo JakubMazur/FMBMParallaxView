2147f0a8660ebd892e4f120f1b04daf279e4f6a3
Kamil Burczyk <kamil.burczyk@sigmapoint.pl>
2014-07-05 17:28:44 +0200

    Improved parallax image view to not to bounce

--- FMBMParallaxView/FMBMImageView.swift
+++ FMBMParallaxView/FMBMImageView.swift
@@ -10,18 +10,57 @@ import UIKit
 
 class FMBMImageView: UIImageView {
 
+    var clipping: Float
+    var horizontalOffset: Float {
+        didSet {
+            let maxV = 1 -  self.frame.size.width / super.image.size.width
+            let maxH = 1 -  self.frame.size.height / super.image.size.height
+            
+            self.layer.contentsRect = CGRectMake(min(maxV, max(0, verticalOffset)), min(maxH, max(0, horizontalOffset)), 1, 1)
+        }
+    }
+    
+    var verticalOffset: Float {
+        didSet {
+            let maxV = 1 -  self.frame.size.width / super.image.size.width
+            let maxH = 1 -  self.frame.size.height / super.image.size.height
+            
+            self.layer.contentsRect = CGRectMake(min(maxV, max(0, verticalOffset)), min(maxH, max(0, horizontalOffset)), 1, 1)
+        }
+    }
+    
+    override var image: UIImage! {
+        get {
+            self.layer.contentsRect = CGRectMake(0, 0, 1, 1)
+            return super.image
+        }
+        set {
+            super.image = image
+        }
+    }
+    
     init(frame: CGRect) {
+        clipping = 0
+        horizontalOffset = 0
+        verticalOffset = 0
+        
         super.init(frame: frame)
-        // Initialization code
+    }
+    
+    init(coder aDecoder: NSCoder!) {
+        clipping = 0
+        horizontalOffset = 0
+        verticalOffset = 0
+        
+        super.init(coder: aDecoder)
     }
 
-    /*
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func drawRect(rect: CGRect)
-    {
-        // Drawing code
+    init(image: UIImage!) {
+        clipping = 0
+        horizontalOffset = 0
+        verticalOffset = 0
+        
+        super.init(image: image)
     }
-    */
 
 }
